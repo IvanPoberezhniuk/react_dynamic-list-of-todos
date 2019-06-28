0c02e206e00b1805817e327afbf50fbb559cc153
Ivan Poberezhnyuk <vanyapoberejnuk@gmail.com>
2019-06-28 14:43:40 +0300

    Updates

--- precache-manifest.4752e7b9fd7bb0eb01c395838cef5420.js
+++ precache-manifest.4752e7b9fd7bb0eb01c395838cef5420.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f07af20b95fa4f5c98368f469d57abaf",
+    "revision": "e4c41dda263a51fbb9ab6cc781af071e",
     "url": "/react_dynamic-list-of-todos/index.html"
   },
   {
-    "revision": "25bae182a70658be43e5",
-    "url": "/react_dynamic-list-of-todos/static/css/main.49858165.chunk.css"
+    "revision": "b912c2e6ae3b08529fac",
+    "url": "/react_dynamic-list-of-todos/static/css/main.6b65a250.chunk.css"
   },
   {
     "revision": "7fb47095f63d35f10c1d",
     "url": "/react_dynamic-list-of-todos/static/js/2.79dfa222.chunk.js"
   },
   {
-    "revision": "25bae182a70658be43e5",
-    "url": "/react_dynamic-list-of-todos/static/js/main.4b2e09e9.chunk.js"
+    "revision": "b912c2e6ae3b08529fac",
+    "url": "/react_dynamic-list-of-todos/static/js/main.4ce374d8.chunk.js"
   },
   {
     "revision": "04977e40ef1cd22bcb98",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_dynamic-list-of-todos/precache-manifest.6ff0d44d32b3b6f8aa839684c31ebc71.js"
+  "/react_dynamic-list-of-todos/precache-manifest.4752e7b9fd7bb0eb01c395838cef5420.js"
 );
 
 self.addEventListener('message', (event) => {
